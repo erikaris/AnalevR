7f98c917fa39200814398d05fb74c295f60edb97
soedomoto <soedomoto@gmail.com>
2019-03-07 09:36:03 +0700

    Move project to github

--- app/html/application/views/templates/footer.php
+++
@@ -1,7 +0,0 @@
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.35.0/codemirror.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.39.0/addon/scroll/simplescrollbars.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.35.0/mode/javascript/javascript.min.js"></script>
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.35.0/mode/r/r.min.js"></script>
-</body>
-
-</html>
